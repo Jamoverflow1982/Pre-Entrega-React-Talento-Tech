b00d41fdde9f034c87f83fc618e740897be2c9e3
Javy <43869579+Jamoverflow1982@users.noreply.github.com>
2025-10-14 00:54:14 -0300

    Primer commit

---
+++ La Tienda ecommerce/src/components/Inicio/Inicio.jsx
@@ -0,0 +1,26 @@
+import style from "./Inicio.module.css"
+import { useEffect, useState } from "react"
+import TarjetaProducto from "../TarjetaProducto/TarjetaProducto"
+
+const Inicio = () => {
+
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+        fetch('http://fakestoreapi.com/products')
+        .then(res => res.json())
+        .then(data => setProductos(data))
+    }, [])
+
+    return (
+        <>
+            <section className={style.inicio}>
+                <h1>Bienvenido a Emporium</h1>
+                <h2>Nuestra Misión: Crear un Verdadero Emporium</h2>
+                <p className={style.parrafo}>La palabra Emporium significa un gran centro de comercio. En la era digital, ese es nuestro objetivo: ser el único destino que necesitas. En lugar de especializarnos en una sola cosa, hemos decidido especializarnos en tu conveniencia. Hemos reunido una vasta y selecta colección de productos de todos los rubros, desde gadgets de última generación hasta artículos atemporales para el hogar. Cada producto en Emporium ha sido elegido por su calidad y valor, asegurando que cada compra sea una inversión inteligente. Queremos que encuentres, sin esfuerzo, todo lo que deseas y necesitas.</p>
+            </section>
+        </>
+    )
+}
+
+export default Inicio
